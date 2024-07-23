69fd68fc7a91f35696c4141d0b3e57611b8cbca9
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-23 18:17:09 +0300

    Add functions for basic calsulations (+, -, *, /)

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const input = document.querySelector("#input");
 const buttons = document.querySelector("#buttons");
 
+let add = (a, b) => a+b;
+let substract = (a, b) => a-b;
+let multiply = (a, b) => a*b;
+let divide = (a, b) => a/b;
+
 let firstNumber;
 let secondNumber;
 let operation;
+
