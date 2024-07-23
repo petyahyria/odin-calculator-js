35d3838fa721d8592955d7685336723c1d924f7a
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-23 20:18:52 +0300

    Add event listener to buttons
    Buttons should add numbers to #input

--- script.js
+++ script.js
@@ -26,3 +26,60 @@ let operate = (operator, a, b) => {
             break;
     }
 }
+
+buttons.addEventListener("click", (e) => {
+        
+        let target = e.target;
+        switch (target.id) {
+            case "btn1":
+                input.value += "1";
+                break;
+            case "btn2":
+                input.value += "2";
+                break;
+            case "btn3":
+                input.value += "3";
+                break;
+            case "btn+":
+                input.value += "+";
+                break;
+            case "btn4":
+                input.value += "4";
+                break;
+            case "btn5":
+                input.value += "5";
+                break;
+            case "btn6":
+                input.value += "6";
+                break;
+            case "btn-":
+                input.value += "-";
+                break;
+            case "btn7":
+                input.value += "7";
+                break;
+            case "btn8":
+                input.value += "8";
+                break;
+            case "btn9":
+                input.value += "9";
+                break;
+            case "btn*":
+                input.value += "*";
+                break;
+            case "btn0":
+                input.value += "0";
+                break;
+            case "btn-c":
+                input.value = input.value.slice(0, -1); 
+                break;
+            case "btn-ac":
+                input.value = "";
+                break;
+            case "btn/":
+                input.value += "/";
+                break;
+        }
+    });
+
+    
