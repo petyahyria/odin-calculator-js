4aea1b1052a3263f91275c615d263c883c7fd050
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-24 09:44:49 +0300

    Add keyevents

--- script.js
+++ script.js
@@ -67,73 +67,131 @@ buttons.addEventListener("mousedown", (e) => {
     }
 })
 
-buttons.addEventListener("mouseup", (e) => {
-        
-        let target = e.target;
-        if(target.id.includes("btn")){
-            target.style.backgroundColor = "#B0C6CE";
-            target.style.border = "2px solid #0A1128";
-        }
-        switch (target.id) {
-            case "btn1":
-                input.value += "1";
-                break;
-            case "btn2":
-                input.value += "2";
-                break;
-            case "btn3":
-                input.value += "3";
-                break;
-            case "btn+":
-                setOperator("+");
-                input.value += "+";
-                break;
-            case "btn4":
-                input.value += "4";
-                break;
-            case "btn5":
-                input.value += "5";
-                break;
-            case "btn6":
-                input.value += "6";
-                break;
-            case "btn-":
-                setOperator("-");
-                input.value += "-";
-                break;
-            case "btn7":
-                input.value += "7";
-                break;
-            case "btn8":
-                input.value += "8";
-                break;
-            case "btn9":
-                input.value += "9";
-                break;
-            case "btn*":
-                setOperator("*");
-                input.value += "*";
-                break;
-            case "btn0":
-                input.value += "0";
-                break;
-            case "btn-c":
-                input.value = input.value.slice(0, -1); 
-                break;
-            case "btn-ac":
-                input.value = "";
-                firstNumber = null;
-                secondNumber = null;
-                operator = null;
-                break;
-            case "btn/":
-                setOperator("/")
-                input.value += "/";
-                break;
-            case "btn-equals":
-                equals();
-                break;
-        }
-    });
+buttons.addEventListener("mouseup", (e) => {      
+    let target = e.target;
+    if(target.id.includes("btn")){
+        target.style.backgroundColor = "#B0C6CE";
+        target.style.border = "2px solid #0A1128";
+    }
+    switch (target.id) {
+        case "btn1":
+            input.value += "1";
+            break;
+        case "btn2":
+            input.value += "2";
+            break;
+        case "btn3":
+            input.value += "3";
+            break;
+        case "btn+":
+            setOperator("+");
+            input.value += "+";
+            break;
+        case "btn4":
+            input.value += "4";
+            break;
+        case "btn5":
+            input.value += "5";
+            break;
+        case "btn6":
+            input.value += "6";
+            break;
+        case "btn-":
+            setOperator("-");
+            input.value += "-";
+            break;
+        case "btn7":
+            input.value += "7";
+            break;
+        case "btn8":
+            input.value += "8";
+            break;
+        case "btn9":
+            input.value += "9";
+            break;
+        case "btn*":
+            setOperator("*");
+            input.value += "*";
+            break;
+        case "btn0":
+            input.value += "0";
+            break;
+        case "btn-c":
+            input.value = input.value.slice(0, -1); 
+            break;
+        case "btn-ac":
+            input.value = "";
+            firstNumber = null;
+            secondNumber = null;
+            operator = null;
+            break;
+        case "btn/":
+            setOperator("/")
+            input.value += "/";
+            break;
+        case "btn-equals":
+            equals();
+            break;
+    }
+});
 
-    
+addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "Backspace":
+            input.value = input.value.slice(0, -1); 
+            break;
+        case "1":
+            input.value += "1";
+            break;
+        case "2":
+            input.value += "2";
+            break;
+        case "3":
+            input.value += "3";
+            break;
+        case "+":
+            setOperator("+");
+            input.value += "+";
+            break;
+        case "4":
+            input.value += "4";
+            break;
+        case "5":
+            input.value += "5";
+            break;
+        case "6":
+            input.value += "6";
+            break;
+        case "-":
+            setOperator("-");
+            input.value += "-";
+            break;
+        case "7":
+            input.value += "7";
+            break;
+        case "8":
+            input.value += "8";
+            break;
+        case "9":
+            input.value += "9";
+            break;
+        case "*":
+            setOperator("*");
+            input.value += "*";
+            break;
+        case "0":
+            input.value += "0";
+            break;
+        case "/":
+            setOperator("/")
+            input.value += "/";
+            break;
+        case "=":
+            equals();
+            break;    
+        case "Enter":
+            equals();
+            break;    
+
+    }
+});
